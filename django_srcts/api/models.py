edbd1dcd47f5d4e94772d34abaf4f777a88e14af
Jean Michel Rouly <michel.rouly@gmail.com>
2014-03-02 00:16:01 -0500

    derp

--- django_srcts/api/models.py
+++ django_srcts/api/models.py
@@ -5,7 +5,7 @@ class App( models.Model ):
     title = models.CharField( blank=False, max_length=100 )
     desc = models.CharField( blank=True, max_length=500 )
     url = models.URLField( blank=False, max_length=200 )
-    image = models.ImageField( upload_to='media/icons/' )
+    image = models.ImageField( upload_to='icons/' )
 
     def __unicode__(self):
         return "%s" % self.title
