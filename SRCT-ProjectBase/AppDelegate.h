2a15be30371ce96c5dddfc7340076bb585554105
Abdullah Abdurrah <iRunner@mid-campus-03285.wireless.unc.edu>
2014-03-01 19:53:03 -0500

    Initial Commit

---
+++ SRCT-ProjectBase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SRCT-ProjectBase
+//
+//  Created by Abdullah Abdurrah on 3/1/14.
+//  Copyright (c) 2014 Abdullah Abdurrah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
