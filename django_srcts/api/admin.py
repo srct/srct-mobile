f1e5e664a4a6492dfe5244e94ef63cd7a74740cb
Jean Michel Rouly <michel.rouly@gmail.com>
2014-08-28 11:20:39 -0400

    Removed all old contents.

--- django_srcts/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from api.models import App
-
-# Register your models here.
-admin.site.register(App)
--- django_srcts/www/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import render, render_to_response
-from django.shortcuts import get_object_or_404, redirect
-
-# Create your views here.
-def home( request ):
-    return render(request, 'index.html', {
-    },
-    )
